ed6120b53c73a851d04de3b1e923562f5797f8b7
jtsQA <jithin26qa@gmail.com>
2023-06-27 10:42:27 +0530

    streams notes update 1

--- src/main/java/org/learn/streams/streamsexercises/IntArrayList.java
+++ src/main/java/org/learn/streams/streamsexercises/IntArrayList.java
@@ -9,19 +9,20 @@ public class IntArrayList {
         List<Integer> intList1 = List.of(1, 2, 3, 4, 5, 6, 8, 8, 7, 6, 5, 4, 3, 2);
         List<Integer> intList2 = List.of(1, -2, 3, 4, 5, -6, 8, -8, -7, 2, 6, 5, 4, -3, 2);
 
-//        sumOfNumbersInArrayList(intList1);
-//        sumOfNumbersInArrayList(intList2);
-//
-//        averageOfNumbersInArrayList(intList1);
-//        averageOfNumbersInArrayList(intList2);
+        sumOfNumbersInArrayList(intList1);
+        sumOfNumbersInArrayList(intList2);
+
+        averageOfNumbersInArrayList(intList1);
+        averageOfNumbersInArrayList(intList2);
 
         squareAndGetAvgForNumbersGreaterThan100(intList1);
 
-//        getNumbersStaringWith(intList2, 2);
-//        getEvenAndOddNumbers(intList1);
-//        getDuplicates(intList1);
+        getNumbersStaringWith(intList2, 2);
+        getEvenAndOddNumbers(intList1);
+        getDuplicates(intList1);
     }
 
+    //    1.  Get the sum of the numbers in a arraylist of integers.
     private static void sumOfNumbersInArrayList(List<Integer> intList) {
         Integer sum1 = intList.stream().reduce(0, (a, b) -> a + b);
         Integer sum2 = intList.stream().reduce(0, Integer::sum);
@@ -30,6 +31,7 @@ public class IntArrayList {
         System.out.println("Sum of numbers using method reference: " + sum2);
     }
 
+    //    2.  Get the average of the numbers in an arraylist of integers.
     private static void averageOfNumbersInArrayList(List<Integer> intList) {
         double average1 = intList.stream().mapToInt(e -> e).average().getAsDouble();
         System.out.println("\nAverage of numbers using average function: " + average1);
@@ -38,6 +40,7 @@ public class IntArrayList {
         System.out.println("Average of numbers using summaryStatistics: " + average2);
     }
 
+    //    3.  For a given arraylist of integers square all the numbers, and get the average of the numbers greater than 100.
     private static void squareAndGetAvgForNumbersGreaterThan100(List<Integer> intList) {
         double calNum1 = intList.stream().map(e -> e * e).filter(e -> e > 100).mapToInt(e -> e).average().getAsDouble();
         System.out.println("\nSquare and get average for numbers greater than 100 using average function: " + calNum1);
@@ -46,11 +49,13 @@ public class IntArrayList {
         System.out.println("\nSquare and get average for numbers greater than 100 using summaryStatistics: " + calNum2);
     }
 
+    //    4.  For the given arraylist of integers get the numbers starting with 2.(It should also consider the negative values)
     private static void getNumbersStaringWith(List<Integer> intList, int n) {
         System.out.println("\nGetNumbersStaringWith '" + n + "' are: ");
         intList.stream().filter(x -> x.toString().startsWith("2") || x.toString().startsWith("-2")).forEach(System.out::println);
     }
 
+    //    5.  From the given arraylist of integers segregate the even and odd numbers.
     private static void getEvenAndOddNumbers(List<Integer> intList) {
         System.out.println("\nEven Numbers: ");
         intList.stream().sorted().filter(x -> x % 2 == 0).forEach(System.out::println);
@@ -58,10 +63,29 @@ public class IntArrayList {
         intList.stream().sorted().filter(x -> x % 2 != 0).forEach(System.out::println);
     }
 
+    //    6.  Get the duplicates from given arraylist of integers.
     private static void getDuplicates(List<Integer> intList) {
         HashSet<Integer> set = new HashSet<>();
         System.out.println("\nDuplicate Numbers: ");
         intList.stream().filter(x -> !set.add(x)).forEach(System.out::println);
     }
+
+//    7.  Get the Max and Min value from given arraylist of integers.
+
+
+//    8.  Sort the numbers in the arraylist of integers (both asc and desc).
+
+
+//    9.  Get the first 5 numbers from given arraylist of integers.
+
+
+//    10. Skip the first 5 numbers from given arraylist of integers.
+
+
+//    11. Get the 2nd highest number form the given arraylist of integers.
+
+
+//    12. Get the 2nd lowest number form the given arraylist of integers.
+
 
 }
